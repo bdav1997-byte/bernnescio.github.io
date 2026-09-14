aada20eaa2bd3dc5d6523a8a8c5df78b8097d41f
bdav1997-byte <bdav.1997@gmail.com>
2026-09-14 14:28:52 +0100

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Pequenas interações do NESCIO.
-// O site funciona sem JavaScript; este ficheiro apenas melhora a experiência.
-
-document.querySelectorAll('nav a, .back-top').forEach(link => {
-  link.addEventListener('click', () => {
-    document.body.classList.add('navigating');
-    setTimeout(() => document.body.classList.remove('navigating'), 500);
-  });
-});
